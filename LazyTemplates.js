4a2aa572c3833d35ce6f563cbb5a8befab144b9f
Charlie Hoover <choover@fathomcreative.com>
2013-01-01 19:46:38 -0500

    refactor to allow for callbacks and setting width and height on placeholder

--- LazyTemplates.js
+++ LazyTemplates.js
@@ -1,60 +1,79 @@
 (function( $ ) {
+  
+window.LazyTemplates = {
+  
+    setWidth: function (_targetElem, _width) {
+        $(_targetElem).parent().css("width", _width)
+    },
+  
+    setHeight: function (_targetElem, _height) {
+        $(_targetElem).parent().css("height", _height)
+    },
+  
+    setCallback: function (_targetElem, _callback) {
+        if ("string" == typeof _callback) $(_targetElem).data("callback", window[_callback]);
+        else if ("function" == typeof _callback) $(_targetElem).data("callback", _callback);
+        else throw "invalid callback provided";
+    },
+  
+    loadVisible: function () {
+        $(".lazy-wrapper").each(function () {
+            LazyTemplates.elementInViewport(this) && LazyTemplates.loadContent($(this).children().first());
+        })
+    },
+  
+    loadContent: function (_targetElem) {
+        $(_targetElem).parent().before($(_targetElem).html());
+      
+        var callback = $(_targetElem).data("callback");
+        "function" == typeof callback && setTimeout(function () {callback();}, 0);
+      
+        $(_targetElem).parent().remove()
+    },
+  
+    wrapTemplates: function () {
+        $("script.lazy").each(function () {
+            $(this).wrap('<div class="lazy-wrapper">');
+          
+            var callback = $(this).attr("data-callback");
+            "undefined" != typeof callback && LazyTemplates.setCallback(this, callback);
+          
+            var width = $(this).attr("data-width");
+            "undefined" != typeof width && LazyTemplates.setWidth(this, width);
+          
+            var height = $(this).attr("data-height");
+            "undefined" != typeof height && LazyTemplates.setHeight(height);
 
-var LazyTemplates = {};
+        })
+    },
+  
+    elementInViewport: function(_targetElem) {
+        var top    = _targetElem.offsetTop,
+            left   = _targetElem.offsetLeft,
+            width  = _targetElem.offsetWidth,
+            height = _targetElem.offsetHeight;
 
-LazyTemplates.loadVisible = function(){
+        while(_targetElem.offsetParent) {
+          _targetElem    = _targetElem.offsetParent;
+          top  += _targetElem.offsetTop;
+          left += _targetElem.offsetLeft;
+        }
 
-   $('.lazy-wrapper').each(function(){  
-      if(LazyTemplates.elementInViewport(this)){
-               LazyTemplates.loadContent($(this).children().first());
-       }                
-           
-   });
+        return (
+          top < (window.pageYOffset + window.innerHeight) &&
+          left < (window.pageXOffset + window.innerWidth) &&
+          (top + height) > window.pageYOffset &&
+          (left + width) > window.pageXOffset
+        );
+      }
+
 };
-    
-LazyTemplates.loadContent = function(_targetElem){
-   $(_targetElem).parent().before($(_targetElem).html());
-   $(_targetElem).parent().remove();
-};
-    
-LazyTemplates.wrapTemplates = function(){
-    $('script.lazy').each(function(){  
-         $(this).wrap('<div class="lazy-wrapper">');                    
-    });
-};  
-    
-LazyTemplates.elementInViewport = function(el) {
-  var top    = el.offsetTop,
-      left   = el.offsetLeft,
-      width  = el.offsetWidth,
-      height = el.offsetHeight;
 
-  //check the parents positioning...
-  while(el.offsetParent) {
-    el    = el.offsetParent;
-    top  += el.offsetTop;
-    left += el.offsetLeft;
-  }
-
-  //Make sure it is within the 
-  //bottom  (y offset + height) right  (x offset + width)
-  //and  top (y offset ) left(x offset) of viewport
-  return (
-    top < (window.pageYOffset + window.innerHeight) &&
-    left < (window.pageXOffset + window.innerWidth) &&
-    (top + height) > window.pageYOffset &&
-    (left + width) > window.pageXOffset
-  );
-};
-    
-    
-$(document).ready(
-    function(){
-        LazyTemplates.wrapTemplates();
-        LazyTemplates.loadVisible();
-
-        $(window).on('resize', LazyTemplates.loadVisible);
-        $(window).on('scroll', LazyTemplates.loadVisible);
- });
+$(document).ready(function () {
+    LazyTemplates.wrapTemplates();
+    LazyTemplates.loadVisible();
+    $(window).on("resize", LazyTemplates.loadVisible);
+    $(window).on("scroll", LazyTemplates.loadVisible)
+});
 
 })( jQuery );
